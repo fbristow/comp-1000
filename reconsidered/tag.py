b39bd245c5c4abf1049a3c062398d6ec0d65b48f
Franklin Bristow <fbristow@cs.umanitoba.ca>
2020-03-25 15:41:44 -0500

    Learning outcomes are colour coded.

--- reconsidered/tag.py
+++ reconsidered/tag.py
@@ -4,7 +4,16 @@ import panflute as pf
 import sys
 from collections import defaultdict 
 
-tags = defaultdict(lambda: [])
+colours = [('black', '#c8c3cc'), ('white', '#563f46'), ('white', '#8ca3a3'), ('white', '#484f4f'), ('black', '#e0e2e4'), ('black', '#c6bcb6'), ('white', '#96897f'), ('white', '#7efa35'), ('black', '#cab577'), ('black', '#dbceb0'), ('white', '#838060'), ('white', '#4f3222')]
+colour_idx = 0
+
+def __default_tuple():
+    global colour_idx
+    v = (colour_idx, [])
+    colour_idx += 1
+    return v
+
+tags = defaultdict(__default_tuple)
 
 def prepare(doc):
     pass
@@ -17,12 +26,15 @@ def action(elem, doc):
         # if it's not an empty string and the text is surrounded by colons
         # (e.g., :likethis:), then it's a tag that we should capture
         if tn > 1 and t[::tn-1] == '::':
-            wrapped = pf.Emph(pf.SmallCaps(elem))
+            tag = tags[t]
+            colour = colours[tag[0]]
+            pf.debug(colour)
+            wrapped = pf.Span(pf.SmallCaps(elem), attributes={'style': f"color:{colour[0]};background-color:{colour[1]};border:1px solid black;"})
             # The tags are written in `pf.Str` elements at the end of a learning
             # objective. The parent is a `pf.Plain` element, and *that* 
             # element's parent is the `pf.ListItem` that I want to append at
             # the end of the document.
-            tags[t].append(elem.parent.parent)
+            tags[t][1].append(elem.parent.parent)
 
     return wrapped
 
@@ -35,7 +47,7 @@ def finalize(doc):
         content.append(pf.Header(pf.Str(text=outcome), level=2, classes=['unnumbered'], identifier=outcome))
         # the list in the dict is already a bunch of `pf.ListItem`, so just add
         # them in a new `pf.OrderedList`
-        content.append(pf.OrderedList(*tags[k]))
+        content.append(pf.OrderedList(*tags[k][1]))
 
 def main(doc=None):
     return pf.run_filter(action, prepare=prepare, finalize=finalize, doc=doc)
