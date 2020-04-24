6849a8d5c96a99c41e8466980e3c1ac044484f56
Franklin Bristow <fbristow@cs.umanitoba.ca>
2020-04-24 15:21:07 -0500

    First half of the course started.

--- reconsidered/nameref.py
+++ reconsidered/nameref.py
@@ -4,7 +4,7 @@ import re
 import panflute as pf
 
 # e.g., [*link] or [=link-id]
-ref_pattern = re.compile("\[[\*=][\w-]+\]")
+ref_pattern = re.compile("\[[\*=~][\w-]+\]")
 
 def prepare(doc):
     pass
@@ -42,13 +42,14 @@ def action(elem, doc):
             if ref_type == '*':
                 # Dereferencing type: fill in the learning objective with a link
                 # back to the original.
-                elems.append(pf.Span(pf.Str(pf.stringify(section)), pf.Space(),
-                        pf.Link(pf.Str(f"({course})"),
-                            url=f"#{section_id}"), attributes={"outcomes": outcomes}))
-            elif ref_type == '=':
-                # Equivalent to, just put a link back to the original.
+                elems.append(pf.Span(section, pf.Space(),
+                        pf.Link(pf.Str(f"({course})"), url=f"#{section_id}"), attributes={
+                                 "source-id": section_id}))
+            elif ref_type in ('=', '~'):
+                # Equivalent to or approximates, just put a link back to the original.
                 elems.append(pf.Space())
-                elems.append(pf.Link(pf.Str(f"(equiv {course}#{section_id})"), url=f"#{section_id}"))
+                elems.append(pf.Link(pf.Str(f"({ref_type}{course})"), url=f"#{section_id}",
+                        title=f"{course}#{section_id}"))
 
         return pf.Plain(*elems)
     
